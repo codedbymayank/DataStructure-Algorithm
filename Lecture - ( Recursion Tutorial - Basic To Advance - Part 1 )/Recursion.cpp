19cfd31a6619f57b5490f7ac7c06e5c0598aaa86
codedbymayank <mayankpal7724@gmail.com>
2025-04-25 08:18:15 +0530

    Lecture - ( Recursion Tutorial - Basic To Advance - Part 1 )

--- Lecture - ( Recursion Tutorial - Basic To Advance - Part 1 )/Recursion.cpp
+++ Lecture - ( Recursion Tutorial - Basic To Advance - Part 1 )/Recursion.cpp
@@ -17,8 +17,31 @@ void printNumber(int no)
     cout << no << " " ;
     printNumber(no-1) ;
 }
+
+int Sum_Of_N_Numbers(int no)
+{
+    
+    if(no==0)
+    {
+        return 0 ;    
+    }
+
+    return no + Sum_Of_N_Numbers(no-1) ;
+}
+
 int main()
 {
     // func1();
     printNumber(5);
+    cout << endl;
+    int result = Sum_Of_N_Numbers(5);
+    cout << "Sum of N Numbers : " << result << endl;
+    /*
+        TC = total calls * WD in each calls
+        TC = n * O(1)
+        TC = O(n)
+        
+        SC = O(n)
+     */
+
 }
